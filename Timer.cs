5651f9254ef827d2bd16e57b9eb5b0d0073d0846
lubomirmyskin <159466375+lubomirmyskin@users.noreply.github.com>
2024-03-21 16:23:29 +0300

    Add files via upload

---
+++ Timer.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+using UnityEngine.WSA;
+
+public class Timer : MonoBehaviour
+{
+    int minute = 1;
+    float second = 0.01f;
+    public string sceneName;
+
+    public void Update()
+    {
+        second -= Time.deltaTime;
+
+        if (second == 0)
+        {
+            if (minute > 0)
+            {
+                minute -= 1;
+                second += 0.59f;
+            }
+            else
+            {
+                EditorSceneManager.LoadScene(sceneName);
+            }
+        }   
+        
+        
+
+    }
+}
